4ef28eb578cb5ece7bec2a5dbd52e1f722896e30
Nehuén Villalba <neguuvill@gmail.com>
2024-11-26 17:17:31 -0300

    Hice el sistema de favoritos.

--- app/views.py
+++ app/views.py
@@ -33,17 +33,22 @@ def search(request):
 # Estas funciones se usan cuando el usuario está logueado en la aplicación.
 @login_required
 def getAllFavouritesByUser(request):
-    favourite_list = []
+    favourite_list = services.getAllFavourites(request)
     return render(request, 'favourites.html', { 'favourite_list': favourite_list })
 
 @login_required
 def saveFavourite(request):
-    pass
+    if request.method == 'POST':
+        services.saveFavourite(request)
+    return redirect('home')
 
 @login_required
 def deleteFavourite(request):
-    pass
+    if request.method == 'POST':
+        services.deleteFavourite(request)
+    return redirect('favoritos')
 
 @login_required
 def exit(request):
-    pass
+    logout(request)
+    return redirect('index-page')
